e61b40fcf80e4170d510246e3a0c3867d5064228
Kskaixang <wss07715@gmail.com>
2025-05-14 16:27:49 +0800

    250514-1627

--- src/main/java/com/example/demo/UserLogoutServlet.java
+++ src/main/java/com/example/demo/UserLogoutServlet.java
@@ -11,7 +11,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 @WebServlet("/user/logout")
-public class UserLogoutServlet extends HttpServlet{
+public class UserLogoutServlet extends HttpServlet{ 
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
